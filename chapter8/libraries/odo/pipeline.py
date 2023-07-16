fe86d2b7739ed15352c72b1b037fa79cbf335b17
e pycharm <emily.schoof@gmail.com>
2023-07-15 22:58:02 -0400

    add libraries directory

--- chapter8/libraries/odo/pipeline.py
+++ chapter8/libraries/odo/pipeline.py
@@ -2,7 +2,7 @@ from odo import odo
 import yaml
 
 # Import pipeline configuration
-with open('../config.yaml', 'r') as file:
+with open('../../config.yaml', 'r') as file:
     config_data = yaml.safe_load(file)
 
 # Define the load_data() function as a pipeline using Odo:
