adb993bfe3a7e9cfe98f14c01bee87c342de6ca1
e pycharm <emily.schoof@gmail.com>
2023-08-15 21:06:21 -0400

    confirmed flow with chapter - odo

--- chapter_08/tools/02_odo_pipeline.py
+++ chapter_08/tools/02_odo_pipeline.py
@@ -1,6 +1,6 @@
 # Import Modules
 import yaml
-from odo import odo
+import odo
 from chapter_08.etl.transform import (
     transform_crash_data,
     transform_vehicle_data,
@@ -15,22 +15,34 @@ with open('../config.yaml', 'r') as file:
 def main():
 
     # Step 1: Extract Data with odo
-    crash_df = odo('data/traffic_crashes.csv', dshape=config_data['extract']['crash_df'])
-    vehicle_df = odo('data/traffic_crash_vehicle.csv', dshape=config_data['extract']['vehicle_df'])
-    people_df = odo('data/traffic_crash_people.csv', dshape=config_data['extract']['people_df'])
+    crash_df = odo.odo('data/traffic_crashes.csv',
+                       dshape=config_data['extract']['crash_df'])
+    vehicle_df = odo.odo('data/traffic_crash_vehicle.csv',
+                         dshape=config_data['extract']['vehicle_df'])
+    people_df = odo.odo('data/traffic_crash_people.csv',
+                        dshape=config_data['extract']['people_df'])
 
     # Step 2: Transform Data
-    transformed_crashes_df = transform_crash_data(crash_df)
-    transformed_vehicle_df = transform_vehicle_data(vehicle_df)
-    transformed_people_df = transform_people_data(people_df)
+    transformed_crashes_df = odo.odo(transform_crash_data(crash_df),
+                                     dshape=config_data['transform']['transformed_crashes_df'])
+    transformed_vehicle_df = odo.odo(transform_vehicle_data(vehicle_df),
+                                     dshape=config_data['transform']['transformed_vehicle_df'])
+    transformed_people_df = odo.odo(transform_people_data(people_df),
+                                     dshape=config_data['transform']['transformed_people_df'])
 
     # Step 3: Load Data to PSQL with odo
-    odo(transformed_crashes_df, config_data['load']['dsn'],
-        table=config_data['load']['tables']['chicago_dmv.CRASH'], if_exists='replace')
-    odo(transformed_vehicle_df, config_data['load']['dsn'],
-        table=config_data['load']['tables']['chicago_dmv.Vehicle'], if_exists='replace')
-    odo(transformed_people_df, config_data['load']['dsn'],
-        table=config_data['load']['tables']['chicago_dmv.PEOPLE'], if_exists='replace')
+    odo.odo(transformed_crashes_df,
+            dshape=config_data['load']['dsn'],
+            table=config_data['load']['tables']['chicago_dmv.CRASH'],
+            if_exists='replace')
+    odo.odo(transformed_vehicle_df,
+            dshape=config_data['load']['dsn'],
+            table=config_data['load']['tables']['chicago_dmv.Vehicle'],
+            if_exists='replace')
+    odo.odo(transformed_people_df,
+            dshape=config_data['load']['dsn'],
+            table=config_data['load']['tables']['chicago_dmv.PEOPLE'],
+            if_exists='replace')
 
 # Call the load_data() function to execute the data pipeline
 if __name__ == '__main__':
